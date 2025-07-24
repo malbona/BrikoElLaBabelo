732343d8880e4c69a6e5f3add45be03fc7070dcc
malbona <41956963+malbona@users.noreply.github.com>
2025-07-24 10:22:59 +0800

    Temporarily remove support for Hebrew and Yiddish & Import support for both traditional and simplified Chinese

--- app/src/main/java/eo/brikoellababelo/MainActivity.kt
+++ app/src/main/java/eo/brikoellababelo/MainActivity.kt
@@ -3,107 +3,34 @@ package eo.brikoellababelo
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.core.os.LocaleListCompat
 import eo.brikoellababelo.ui.content.Greeting
+import eo.brikoellababelo.ui.content.LocaleDropdownMenu
 import eo.brikoellababelo.ui.theme.BrikoElLaBabeloTheme
 
 class MainActivity : AppCompatActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val config = resources.configuration
-//        val lang = "zh" // your language code
-//        val locale = Locale(lang)
-//        Locale.setDefault(locale)
-//        config.setLocale(locale)
-//        resources.updateConfiguration(config, resources.displayMetrics)
 
         setContent {
             BrikoElLaBabeloTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     Column {
                         LocaleDropdownMenu()
                         Greeting(stringResource(id = R.string.app_name))
                     }
                 }
-            }
-        }
-    }
-}
-
-@ExperimentalMaterial3Api
-@Composable
-fun LocaleDropdownMenu() {
-    val localeOptions = mapOf(
-        R.string.en to "en",
-        R.string.fr to "fr",
-        R.string.eo to "eo",
-        R.string.uk to "uk",
-        R.string.fi to "fi",
-        R.string.zh to "zh",
-        R.string.ji to "hi",
-        R.string.iw to "iw"
-    ).mapKeys { stringResource(it.key) }
-
-    // boilerplate: https://developer.android.com/reference/kotlin/androidx/compose/material/package-summary#ExposedDropdownMenuBox(kotlin.Boolean,kotlin.Function1,androidx.compose.ui.Modifier,kotlin.Function1)
-    var expanded by remember { mutableStateOf(false) }
-    ExposedDropdownMenuBox(
-        expanded = expanded,
-        onExpandedChange = {
-            expanded = !expanded
-        }
-    ) {
-        TextField(
-            modifier = Modifier.menuAnchor(),
-            readOnly = true,
-            value = stringResource(R.string.language),
-            onValueChange = { },
-            trailingIcon = {
-                ExposedDropdownMenuDefaults.TrailingIcon(
-                    expanded = expanded
-                )
-            }
-        )
-        ExposedDropdownMenu(
-            expanded = expanded,
-            onDismissRequest = {
-                expanded = false
-            }
-        ) {
-            localeOptions.keys.forEach { selectionLocale ->
-                DropdownMenuItem(
-                    onClick = {
-                        expanded = false
-                        // set app locale given the user's selected locale
-                        AppCompatDelegate.setApplicationLocales(
-                            LocaleListCompat.forLanguageTags(
-                                localeOptions[selectionLocale]
-                            )
-                        )
-                    },
-                    text = { Text(selectionLocale) }
-                )
             }
         }
     }
--- app/src/main/java/eo/brikoellababelo/ui/content/Screen.kt
+++ app/src/main/java/eo/brikoellababelo/ui/content/Screen.kt
@@ -1,25 +1,70 @@
 package eo.brikoellababelo.ui.content
 
+import androidx.appcompat.app.AppCompatDelegate.setApplicationLocales
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults.TrailingIcon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import eo.brikoellababelo.ui.theme.BrikoElLaBabeloTheme
+import androidx.core.os.LocaleListCompat.forLanguageTags
+import eo.brikoellababelo.R
 
 @Composable
 fun Greeting(name: String) {
     Row {
-        Text(modifier = Modifier.padding(start = 50.dp), text = name)
+        Text(
+            modifier = Modifier.padding(start = 50.dp),
+            text = name
+        )
     }
 }
 
-@Preview(showBackground = true)
+@ExperimentalMaterial3Api
 @Composable
-fun DefaultPreview() {
-    BrikoElLaBabeloTheme {
-        Greeting("Android")
+fun LocaleDropdownMenu() {
+    val localeOptions = mapOf(
+        R.string.en_name to stringResource(id = R.string.en_code),
+        R.string.eo_name to stringResource(id = R.string.eo_code),
+        R.string.fi_name to stringResource(id = R.string.fi_code),
+        R.string.fr_name to stringResource(id = R.string.fr_code),
+        R.string.uk_name to stringResource(id = R.string.uk_code),
+        R.string.zh_Hans_name to stringResource(id = R.string.zh_Hans_code),
+        R.string.zh_Hant_name to stringResource(id = R.string.zh_Hant_code)
+    ).mapKeys { stringResource(it.key) }
+    var expanded by remember { mutableStateOf(false) }
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded }
+    ) {
+        TextField(modifier = Modifier.menuAnchor(),
+            readOnly = true,
+            value = stringResource(R.string.language),
+            onValueChange = { },
+            trailingIcon = { TrailingIcon(expanded = expanded) })
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            localeOptions.keys.forEach { selectionLocale ->
+                DropdownMenuItem(
+                    onClick = {
+                        expanded = false
+                        setApplicationLocales(forLanguageTags(localeOptions[selectionLocale]))
+                    },
+                    text = { Text(selectionLocale) }
+                )
+            }
+        }
     }
 }
